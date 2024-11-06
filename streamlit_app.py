fd84f3b4c9d6a4f747d885ed848ada94e036c733
Jonah Eagleson <jonaheagleson@gmail.com>
2024-11-06 14:51:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ with st.sidebar:
             'bill_depth_mm': bill_depth_mm, 
             'flipper_length_mm': flipper_length_mm, 
             'body_mass_g': body_mass_g, 
-            'sec': gender}
+            'sex': gender}
     input_df = pd.DataFrame(data, index=[0])
     input_penguins = pd.concat([input_df, X], axis=0)
 
