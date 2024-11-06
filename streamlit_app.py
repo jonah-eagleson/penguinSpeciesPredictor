1fab8f9382c36ab952c8994498246645094536c6
Jonah Eagleson <30636178+jonah-eagleson@users.noreply.github.com>
2024-11-06 14:20:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ st.title('Penguin Species Predictor')
 st.info("This app utilizes a machine learning model to predict penguin species")
 
 with st.expander('Data'):
-    st.write('**RAW data')
-    df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+    st.write('**RAW data**')
+    df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
     df
