0591690718615ac7319b0c788d7e088aaefd3621
Jonah Eagleson <jonaheagleson@gmail.com>
2024-11-06 15:07:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sklearn
+from sklearn.ensemble import RandomForestClassifier
 import numpy
 
 st.title('Penguin Species Predictor')
@@ -53,6 +53,8 @@ with st.expander('Input Features'):
 # Encode X
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+X = df_penguins[1:]
 input_row = df_penguins[:1]
 
 # Encode y
@@ -70,3 +72,11 @@ with st.expander("Data Preperation"):
     input_row
     st.write("**Encoded y**")
     y
+
+# Model Training
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+# Apply model to make predictions
+prediction = clf.predict(input_row)
+prediction_prob = clf.predict_proba(input_row)
